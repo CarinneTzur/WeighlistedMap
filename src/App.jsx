fb294b479af3e8bf9d51c4ca70fbda3d509cef7b
CarinneTzur <ctzurdecker@outlook.com>
2026-05-14 21:20:19 -0400

    Remove unused marker cluster imports
    Removed unused imports for marker clustering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "leaflet.markercluster/dist/MarkerCluster.css";
-import "leaflet.markercluster/dist/MarkerCluster.Default.css";
-import "leaflet.markercluster";
 import {
 	getAllCoaches,
 	getStateByAbbr,
